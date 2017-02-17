a170a36fd2068d9bb543045d0529f88caf42e6b6
filippov70 <filippov70@gmail.com>
2017-02-17 19:26:13 +0700

    Заготовка вёрстки тестовой страницы.

---
+++ CopterMap/public_html/app.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function () {
+    var config = {
+        layer01 = "",
+        layer02 = "",
+        layer03 = "",
+        lauer04 = ""
+    }
+    
+    
+});
